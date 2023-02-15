1d4cc9cea4cd07da9b0d7982021ed8f18fe527c7
favioag <favioag@gmail.com>
2023-02-15 16:54:39 -0300

    nav update

--- src/components/FormSp/index.jsx
+++ src/components/FormSp/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
+import './form.css'
 function FormSp() {
-  
-    return (
-        <form action="https://formspree.io/f/mpzepjqa" method="POST">
-        <input type="text" name="name" placeholder="Your Name" />
-        <input type="email" name="email" placeholder="Your Email" />
-        <textarea name="message" placeholder="Your Message"></textarea>
-        <button type="submit">Send</button>
-      </form>
-    )
- 
-  
-  
+  return (
+    <div className='formCont'>
+      <div>
+        <h1>Contactame!</h1>
+        <h2>Estoy para escucharte! Dejame tu comentario o duda en el siguiente formulario</h2>
+        <h3>¿Como puedo ayudarte?</h3>
+      </div>
+        <form action="https://formspree.io/f/mpzepjqa" method="POST" className='form'>
+          <input type="text" name="name" placeholder="Your Name" className='name' />
+          <input type="email" name="email" placeholder="Your Email" className='name'/>
+          <textarea name="message" placeholder="Your Message" className='message'></textarea>
+          <button type="submit">Send</button>
+        </form>
+    </div>
+  );
 }
 
-export {FormSp}
+export { FormSp };
